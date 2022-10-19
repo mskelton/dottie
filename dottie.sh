b7c732e0a26e65e65530e05c3b1a7f0a357f6cfa
Mark Skelton <mark.skelton@acquia.com>
2022-10-19 17:35:00 -0500

    Quote paths

--- dottie.sh
+++ dottie.sh
@@ -16,28 +16,28 @@ function format_path {
 # Cleans up an old file or symlink
 function clean {
 	if [[ -f "$1" ]]; then
-		rm $1
-		echo -e "${YELLOW}Cleaning $(format_path $1)${NC}"
+		rm "$1"
+		echo -e "${YELLOW}Cleaning $(format_path "$1")${NC}"
 	else
-		echo -e "${BLUE}File already cleaned $(format_path $1)${NC}"
+		echo -e "${BLUE}File already cleaned $(format_path "$1")${NC}"
 	fi
 }
 
 # Symlinks a file or directory
 function link {
 	local src=$(pwd)/$1
-	local dest=$2
+	local dest="$2"
 	local formatted_dest="${dest/$HOME/~}"
 
 	# If the file already exists and is a symlink to the correct source location,
 	# skip linking.
-	if [[ $(readlink -f "$dest") == $src ]]; then
+	if [[ $(readlink -f "$dest") == "$src" ]]; then
 		echo -e "${BLUE}Link exists $1 -> $formatted_dest${NC}"
 		return
 	fi
 
 	# If the file already exists and is a regular file, mark as an error
-	if [[ -e $dest ]]; then
+	if [[ -e "$dest" ]]; then
 		echo -e "${RED}$formatted_dest already exists but is a regular file or directory${NC}"
 		__dottie_error="y"
 		return
@@ -45,15 +45,15 @@ function link {
 
 	# All good, create the link and any parent directories if necessary
 	echo -e "${GREEN}Creating link $1 -> $formatted_dest${NC}"
-	mkdir -p $(dirname $dest)
-	ln -s $src $dest
+	mkdir -p $(dirname "$dest")
+	ln -s "$src" "$dest"
 }
 
 # Links the contents of a directory and it's subdirectories. This is primarily
 # needed for tools such as Fish where the entire directory can't be linked due
 # to auto-generated files or other files that cannot be committed to VCS.
 function link_contents {
-	for file in $(find $1 -name "*" ! -name '.DS_Store' -type f); do
+	for file in $(find "$1" -name "*" ! -name '.DS_Store' -type f); do
 		link $file "$2${file/$1\//}"
 	done
 }
@@ -61,7 +61,7 @@ function link_contents {
 # If any of the links failed, log a final message indicating something went
 # wrong. This helps when the install log gets long.
 function finalize {
-	if [[ -n $error ]]; then
+	if [[ -n $__dottie_error ]]; then
 		echo ""
 		echo -e "${RED}Some tasks were not successful!${NC}"
 		exit 1
